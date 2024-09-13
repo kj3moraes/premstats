df56731c29978b9cb6701addae18ff5554d889f4
Keane Moraes <lordvader3002@gmail.com>
2024-09-13 23:40:57 +0800

    added more tests for teams

--- backend/app/tests/api/routes/test_team.py
+++ backend/app/tests/api/routes/test_team.py
@@ -39,3 +39,44 @@ def test_read_item_not_found(client: TestClient) -> None:
     assert response.status_code == 404
     content = response.json()
     assert content["detail"] == "Team not found"
+
+
+def test_uniqueness_name(client: TestClient) -> None:
+    data = {"name": "The Fremen"}
+    response = client.post(
+        "/api/team/add",
+        json=data,
+    )
+    assert response.status_code == 201
+
+    response = client.post(
+        "/api/team/add",
+        json=data,
+    )
+    assert response.status_code == 409
+
+
+def test_team_add_request_validation(client: TestClient) -> None:
+    data = {"nme": "Harkonnens"}
+    response = client.post(
+        "/api/team/add",
+        json=data,
+    )
+
+    assert response.status_code == 422
+
+
+def test_team_update(client: TestClient) -> None:
+    data = {"name": "Atreides"}
+    response = client.post(
+        "/api/team/add",
+        json=data,
+    )
+    assert response.status_code == 201
+    content = response.json()
+    team_id = content["id"]
+    print(type(team_id))
+
+    updated_data = {"id": team_id, "name": "Dead"}
+    response = client.put(f"/api/team/update/{team_id}", json=updated_data)
+    assert response.status_code == 200
