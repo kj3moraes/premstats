8b6ebfdcbd985b048de652a286d9b7a9d695736e
Keane Moraes <lordvader3002@gmail.com>
2024-09-26 21:08:28 +0800

    made changes for accomodating vertical and horizontal

--- premstats-frontend/api/query.ts
+++ premstats-frontend/api/query.ts
@@ -1,34 +1,34 @@
 interface BackendRequest {
-    message: string 
+  message: string;
 }
 
 interface BackendResponse {
-    message: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: Array<{ [key: string]: any }> | number | string;
+  message: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: Array<{ [key: string]: any }> | number | string;
 }
 
 export const query_backend = async (query: string): Promise<string> => {
-    // Construct the search request body
-    const requestBody: BackendRequest = {
-        message: query
-    };
+  // Construct the search request body
+  const requestBody: BackendRequest = {
+    message: query,
+  };
 
-    console.log(requestBody)
-    // Send a POST request to the search API
-    console.log(process.env.BACKEND_API_URL)
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const api_query_path = process.env.BACKEND_API_URL + "/api/query/ask_stats"
-    const response = await fetch(`http://localhost:8000/api/query/ask_stats`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-    });
-    
-    const jsonResponse: BackendResponse = await response.json();
-    console.log(jsonResponse) 
-    
-    return jsonResponse.message;
-}
+  console.log(requestBody);
+  // Send a POST request to the search API
+  console.log(process.env.BACKEND_API_URL);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const api_query_path = process.env.BACKEND_API_URL + "/api/query/ask_stats";
+  const response = await fetch(`http://localhost:8000/api/query/ask_stats`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  const jsonResponse: BackendResponse = await response.json();
+  console.log(jsonResponse);
+
+  return jsonResponse.message;
+};
--- premstats-frontend/app/page.tsx
+++ premstats-frontend/app/page.tsx
@@ -1,83 +1,79 @@
-"use client"
+"use client";
+import { Input } from "@/components/ui/input";
+import Footer from "@/components/footer";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-import React, { useState } from 'react';
-import { Box, Container, Heading, Input, Text, InputGroup, InputRightElement, Icon, Flex, ChakraProvider, VStack, HStack, Center } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
 import { FaFutbol } from "react-icons/fa";
-import { query_backend } from '../api/query';
+import { query_backend } from "../api/query";
 
 export default function Home() {
   const [response, setResponse] = useState<string>("");
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!query.trim()) return;
     setIsLoading(true);
+    console.log("Querying backend with:", query);
+    console.log(process.env.BACKEND_API_URL);
     try {
       const response = await query_backend(query);
       setResponse(response);
     } catch (error) {
-      console.error('Error querying backend:', error);
-      setResponse( "An error occurred while fetching the response.");
+      console.error("Error querying backend:", error);
+      setResponse("An error occurred while fetching the response.");
     } finally {
       setIsLoading(false);
-      setQuery('');
+      setQuery("");
     }
   };
 
   return (
-    <ChakraProvider>
-      <Box as="main" minHeight="100vh" bg="white">
-        <Container maxW="container.xl" py={8} height="100vh" display="flex" flexDirection="column">
-          <HStack spacing={8} height="100%" alignItems="flex-start">
-            {/* Left side */}
-            <VStack align="stretch" width="50%" height="100%" justifyContent="center">
-              <Center flexDirection="column">
-                <Flex alignItems="center" mb={8}>
-                  <Icon as={FaFutbol} boxSize={12} mr={4} color="black" />
-                  <Heading as="h1" size="3xl" color="black">
-                    premstats.xyz
-                  </Heading>
-                </Flex>
-                <Box width="100%" maxWidth="500px">
-                  <form onSubmit={handleSubmit}>
-                    <InputGroup size="lg">
-                      <Input
-                        pr="4.5rem"
-                        placeholder="Ask about Premier League stats..."
-                        _placeholder={{ color: 'gray.500' }}
-                        borderColor="gray.300"
-                        bg="gray.100"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                        isDisabled={isLoading}
-                      />
-                      <InputRightElement width="4.5rem">
-                        <ArrowForwardIcon
-                          color={isLoading ? "gray.300" : "gray.500"}
-                          boxSize={6}
-                          cursor={isLoading ? "not-allowed" : "pointer"}
-                          onClick={handleSubmit}
-                        />
-                      </InputRightElement>
-                    </InputGroup>
-                  </form>
-                  <Text color="gray.500" fontSize="sm" mt={2} textAlign="center">
-                    You can ask about any Premier League statistic up to but not including the current season
-                  </Text>
-                </Box>
-              </Center>
-            </VStack>
-            {/* Right side */}
-            <VStack align="stretch" width="50%" spacing={4} p={4} borderRadius="md" height="100%" overflowY="auto">
-              <Text color="gray.800">{response}</Text>
-              {isLoading && <Text color="gray.500">Loading...</Text>}
-            </VStack>
-          </HStack>
-        </Container>
-      </Box>
-    </ChakraProvider>
+    <div className="min-h-screen bg-background p-4 md:p-8 flex items-center justify-center">
+      <div className="w-full max-w-6xl flex flex-col md:flex-row md:items-center md:justify-between">
+        {/* Left Side */}
+        <div className="md:w-1/2 mb-8 md:mb-0 flex flex-col items-center md:items-start space-y-2">
+          <div className="flex flex-row space-x-4 justify-center">
+            <FaFutbol className="text-5xl text-primary-background" />
+            <h1 className="text-5xl font-bold mb-4 text-center md:text-left">
+              premstats.xyz
+            </h1>
+          </div>
+          <form onSubmit={handleSubmit} className="w-full max-w-md">
+            <Input
+              type="text"
+              placeholder="Ask about any match or team stats..."
+              className="w-full mb-2"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            {/* <Button type="submit" className="w-full">
+              Submit
+            </Button> */}
+            <p className="text-sm text-muted-foreground mt-2 text-center md:text-left">
+              You can ask about any Premier League season up to but not
+              including the current season.
+            </p>
+          </form>
+          <div>
+            <Alert className="w-full max-w-md">
+              <AlertTitle className='text-lg font-semibold'>Heads up!</AlertTitle>
+              <AlertDescription>
+                This site is in beta mode and may not be fully functional. Please 
+                report any issues to the developer.
+              </AlertDescription>
+            </Alert>
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="md:w-1/2 p-4 rounded-lg">
+          <p>{response}</p>
+          {isLoading && <p>Loading...</p>}
+        </div>
+      </div>
+    </div>
   );
 }
