1e7434161decb483f2f46fad3377eb066b87c188
Keane Moraes <lordvader3002@gmail.com>
2024-10-02 17:45:48 +0800

    loading animation

--- premstats-frontend/app/page.tsx
+++ premstats-frontend/app/page.tsx
@@ -77,7 +77,11 @@ export default function Home() {
         {/* Right side */}
         <div className='rounded-lg p-4 md:w-1/2'> 
           {response && <p>{response}</p>}
-          {isLoading && <p>Loading...</p>}
+          {isLoading && (
+            <div className="flex justify-center items-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+          )}
         </div>
       </div>
       <Toaster />
