543f75bd24b8af687fcf53ae5fb69f085bbda25a
Keane Moraes <lordvader3002@gmail.com>
2024-10-08 01:29:16 +0800

    added a table

--- premstats-frontend/components/info-button.tsx
+++ premstats-frontend/components/info-button.tsx
@@ -26,20 +26,47 @@ export default function MoreInfoButton({ responseData }: MoreInfoButtonProps) {
       <Sheet>
         <SheetTrigger>
           <Button variant='accent' onClick={handleToggle}>
-            {showDictionary ? 'Hide' : 'Show full data'}
+             Show full data
           </Button>
         </SheetTrigger>
         <SheetContent
           side={window.innerWidth >= 768 ? 'right' : 'bottom'}
-          className='flex flex-col'
+          className='xl:w-1/3 xl:max-w-none sm:w-1/2 sm:max-w-none'
         >
           <div className='overflow-auto'>
             <SheetHeader>
               <SheetTitle>All the data</SheetTitle>
               <SheetDescription>
-                    <pre className='whitespace-pre-wrap text-sm text-gray-800'>
-                      {JSON.stringify(responseData.data)}
-                    </pre>
+                  <table className='min-w-full table-auto border-collapse'>
+                    <thead>
+                      <tr>
+                        {Object.keys(responseData.data[0] || {}).map((key) => (
+                          <th
+                            key={key}
+                            className='border bg-gray-300 px-4 py-2 text-left'
+                          >
+                            {key}
+                          </th>
+                        ))}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {responseData.data.map((item, index) => (
+                        <tr
+                          key={index}
+                          className={
+                            index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'
+                          }
+                        >
+                          {Object.values(item).map((value, i) => (
+                            <td key={i} className='border px-4 py-2'>
+                              {value}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
               </SheetDescription>
             </SheetHeader>
           </div>
