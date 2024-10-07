e0a2412a4eca3feb7eb4678df8bc031a1c80f5a8
Keane Moraes <lordvader3002@gmail.com>
2024-10-08 02:26:52 +0800

    formatted changes

--- premstats-frontend/app/page.tsx
+++ premstats-frontend/app/page.tsx
@@ -9,7 +9,7 @@ import { useToast } from '@/hooks/use-toast';
 import type { BackendResponse, SuccessResponse } from '@/lib/query';
 import SuggestionButton from '@/components/suggestion-button';
 import MoreInfoButton from '@/components/info-button';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
 
 export default function Home() {
   const [response, setResponse] = useState<BackendResponse | null>(null); // Initialize as null to avoid showing text
@@ -110,7 +110,9 @@ export default function Home() {
             response && (
               <>
                 <div className='space-y-2'>
-                  <ReactMarkdown>{(response as SuccessResponse).message}</ReactMarkdown>
+                  <ReactMarkdown>
+                    {(response as SuccessResponse).message}
+                  </ReactMarkdown>
                   <MoreInfoButton responseData={response as SuccessResponse} />
                 </div>
               </>
