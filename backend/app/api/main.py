81927820286cdf6ac071807d0dd384a89d93282b
Keane Moraes <lordvader3002@gmail.com>
2024-11-15 21:16:17 +0800

    added the stadium api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from app.api.querying import stats
-from app.api.routes import match, referee, season, team
+from app.api.routes import match, referee, season, stadium, team
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(season.router, prefix="/season", tags=["seasons"])
 api_router.include_router(team.router, prefix="/team", tags=["teams"])
+api_router.include_router(stadium.router, prefix="/stadium", tags=["stadiums"])
 api_router.include_router(match.router, prefix="/match", tags=["matches"])
 api_router.include_router(referee.router, prefix="/referee", tags=["referees"])
 api_router.include_router(stats.router, prefix="/query", tags=["querying"])
